8f942c0949dde988f1bc977ac7608b9486055468
Brian Lovin <briandlovin@gmail.com>
2022-04-27 09:00:54 -0700

    Clean up support imports

--- cypress/integration/home_spec.ts
+++ cypress/integration/home_spec.ts
@@ -1,52 +1,42 @@
 /// <reference types="cypress" />
 
-import supporters, { golden } from '../../src/config/supporters';
+import { golden } from '../../src/config/supporters';
 import team from '../../src/config/team';
 
 describe('Home', () => {
-  before(() => {
-    cy.visit('/');
-  });
+	before(() => {
+		cy.visit('/');
+	});
 
-  it('should render latest episode', () => {
-    cy.get(`[data-cy="latest-episode"]`).should('be.visible');
-  });
+	it('should render latest episode', () => {
+		cy.get(`[data-cy="latest-episode"]`).should('be.visible');
+	});
 
-  it('should render subscription options', () => {
-    cy.fixture('subscription-options').then(subscriptions => {
-      subscriptions.map((subscription: string) =>
-        cy.get(`[data-cy="subscription-${subscription}"]`).should('be.visible')
-      );
-    });
-  });
+	it('should render subscription options', () => {
+		cy.fixture('subscription-options').then((subscriptions) => {
+			subscriptions.map((subscription: string) =>
+				cy.get(`[data-cy="subscription-${subscription}"]`).should('be.visible')
+			);
+		});
+	});
 
-  it('should render suggested episodes', () => {
-    cy.get(`[data-cy="suggested-episodes"]`).should('be.visible');
-  });
+	it('should render suggested episodes', () => {
+		cy.get(`[data-cy="suggested-episodes"]`).should('be.visible');
+	});
 
-  it('should render golden microphones', () => {
-    golden.map(gold =>
-      cy.get(`[data-cy="golden-${gold.name}"]`).should('be.visible')
-    );
-  });
+	it('should render golden microphones', () => {
+		golden.map((gold) => cy.get(`[data-cy="golden-${gold.name}"]`).should('be.visible'));
+	});
 
-  it('should render supporters', () => {
-    supporters.map(supporter =>
-      cy.get(`[data-cy="supporter-${supporter.name}"]`).should('be.visible')
-    );
-  });
+	it('should render question button', () => {
+		cy.get(`[data-cy="question-button"]`).should('be.visible');
+	});
 
-  it('should render question button', () => {
-    cy.get(`[data-cy="question-button"]`).should('be.visible');
-  });
+	it('should render follow on twitter button', () => {
+		cy.get(`[data-cy="follow-button"]`).should('be.visible');
+	});
 
-  it('should render follow on twitter button', () => {
-    cy.get(`[data-cy="follow-button"]`).should('be.visible');
-  });
-
-  it('should render team members', () => {
-    team.map(team =>
-      cy.get(`[data-cy="team-${team.name}"]`).should('be.visible')
-    );
-  });
+	it('should render team members', () => {
+		team.map((team) => cy.get(`[data-cy="team-${team.name}"]`).should('be.visible'));
+	});
 });
